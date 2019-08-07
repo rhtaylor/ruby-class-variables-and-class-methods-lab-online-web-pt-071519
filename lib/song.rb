fbba799c993321e053602ddca23231fcc6f52644
Randy Taylor <randyisbusy@gmail.com>
2019-08-07 19:57:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,14 +33,14 @@ class Song
         end
       new_hash
   end
-  def self.artist_count 
+  def self.artist_count
     new_hash = {}
     @@artists.each do |key|
       if new_hash[key]
-        new_hash[key] += 1 
-      else 
-        new_hahs[key] += 1 
-      end 
+        new_hash[key] += 1
+      else
+        new_hahs[key] += 1
+      end
       new_hash
     end
 end
