bc5ce57096bd2012343b1c1bf5a43e2d67952a5f
Randy Taylor <randyisbusy@gmail.com>
2019-08-07 19:38:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,8 +2,11 @@ class Song
   attr_accessor :name, :artist, :genre
   @@count = 0
   @@artist = []
-  @@genre = []
+  @@genres = []
   def initialize(name, artist, genre)
+    @@count += 1
+    @@artist << artist 
+    @@genres << genre
     @name = name
     @artist = artist
     @genre = genre
